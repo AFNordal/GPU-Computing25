97a0a4fb99af88c5ccaf20a4c0fc8ceb44d3e8a8
Rasmus A. F. Nordal <nordalrasmus01@gmail.com>
2025-04-15 13:39:43 +0200

    ex1 done

--- laboratories/Laboratory_II/ex1/stack_n_heap.c
+++ laboratories/Laboratory_II/ex1/stack_n_heap.c
@@ -8,23 +8,25 @@
 #define N 5
 #define M 5
 
+#define PRINT_RESULT_VECTOR(V, NAME)  \
+    {                                 \
+        printf("%2s: ", NAME);        \
+        for (int i = 0; i < LEN; i++) \
+            printf("%4d ", V[i]);     \
+        printf("\n");                 \
+    }
 
-#define PRINT_RESULT_VECTOR( V, NAME ) {    \
-    printf("%2s: ", NAME);                  \
-    for (int i=0; i<LEN; i++)               \
-        printf("%4d ", V[i]);               \
-    printf("\n");                           \
-}
-
-#define PRINT_RESULT_MATRIX(MAT, NAME) {    \
-    printf("%2s matrix:\n\t", NAME);        \
-    for (int i=0; i<N; i++) {               \
-        for (int j=0; j<M; j++)             \
-            printf("%4d ", MAT[i*M+j]);     \
-        printf("\n\t");                     \
-    }                                       \
-    printf("\n");                           \
-}
+#define PRINT_RESULT_MATRIX(MAT, NAME)          \
+    {                                           \
+        printf("%2s matrix:\n\t", NAME);        \
+        for (int i = 0; i < N; i++)             \
+        {                                       \
+            for (int j = 0; j < M; j++)         \
+                printf("%4d ", MAT[i * M + j]); \
+            printf("\n\t");                     \
+        }                                       \
+        printf("\n");                           \
+    }
 
 // -------- uncomment these two lines when solutions are published --------
 // #include "solutions/stack_n_heap_solutions.c"
@@ -32,41 +34,101 @@
 // ------------------------------------------------------------------------
 
 #ifndef SOLUTION_STACKVEC_1
-#define SOLUTION_STACKVEC_1 { }
+#define SOLUTION_STACKVEC_1 \
+    {                       \
+    }
 #endif
 
 #ifndef SOLUTION_HEAPVEC_1
-#define SOLUTION_HEAPVEC_1 { }
+#define SOLUTION_HEAPVEC_1 \
+    {                      \
+    }
 #endif
 
 #ifndef SOLUTION_HEAPVEC_2
-#define SOLUTION_HEAPVEC_2 { }
+#define SOLUTION_HEAPVEC_2 \
+    {                      \
+    }
 #endif
 
 #ifndef SOLUTION_STACKVEC_2
-#define SOLUTION_STACKVEC_2 { }
+#define SOLUTION_STACKVEC_2 \
+    {                       \
+    }
 #endif
 
 #ifndef SOLUTION_STACKMAT_1
-#define SOLUTION_STACKMAT_1 { }
+#define SOLUTION_STACKMAT_1 \
+    {                       \
+    }
 #endif
 
 #ifndef SOLUTION_HEAPMAT_1
-#define SOLUTION_HEAPMAT_1 { }
+#define SOLUTION_HEAPMAT_1 \
+    {                      \
+    }
 #endif
 
 #ifndef SOLUTION_HEAPMAT_2
-#define SOLUTION_HEAPMAT_2 { }
+#define SOLUTION_HEAPMAT_2 \
+    {                      \
+    }
 #endif
 
 #ifndef SOLUTION_STACKMAT_2
-#define SOLUTION_STACKMAT_2 { }
+#define SOLUTION_STACKMAT_2 \
+    {                       \
+    }
 #endif
 
-int main(void) {
+void heap_vecs_1_aux(int *a, int *b, int *c)
+{
+    for (int i = 0; i < LEN; i++)
+    {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void heap_vecs_2_aux(int *a, int *b, int **c)
+{
+    *c = (int *)malloc(sizeof(int) * LEN);
+    for (int i = 0; i < LEN; i++)
+    {
+        (*c)[i] = a[i] + b[i];
+    }
+}
+
+// int *heap_vecs_3_aux(int *a, int *b)
+// {
+//     int c[LEN];
+//     for (int i = 0; i < LEN; i++)
+//     {
+//         c[i] = a[i] + b[i];
+//     }
+//     return c;
+// }
+
+void heap_mat_1_aux(int *A, int *B, int *C)
+{
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            C[i * M + j] = A[i * M + j] + B[i * M + j];
+}
+
+void heap_mat_2_aux(int *A, int *B, int **C)
+{
+    *C = (int *)malloc(sizeof(int) * N * M);
+    for (int i = 0; i < N * M; i++)
+        for (int j = 0; j < M; j++)
+            (*C)[i * M + j] = A[i * M + j] + B[i * M + j];
+}
+
+int main(void)
+{
     // ---------- for timing ----------
     float CPU_times[NPROBS];
-    for (int i=0; i<NPROBS; i++)
+    for (int i = 0; i < NPROBS; i++)
         CPU_times[i] = 0.0;
     struct timeval temp_1, temp_2;
     // --------------------------------
@@ -83,13 +145,26 @@ int main(void) {
      */
 
 #ifdef RESULTS
-        SOLUTION_STACKVEC_1
-        PRINT_RESULT_VECTOR(c, "c")
+    SOLUTION_STACKVEC_1
+    PRINT_RESULT_VECTOR(c, "c")
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    gettimeofday(&temp_1, (struct timezone *)0);
+    int a[LEN], b[LEN], c[LEN];
+    for (int i = 0; i < LEN; i++)
+    {
+        a[i] = i;
+        b[i] = 100 * i;
+    }
+    for (int i = 0; i < LEN; i++)
+    {
+        c[i] = a[i] + b[i];
+    }
+    gettimeofday(&temp_2, (struct timezone *)0);
+    CPU_times[0] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    PRINT_RESULT_VECTOR(c, "c");
 
 #endif
     // ---------------------- Heap vectors 1 -----------------------
@@ -100,10 +175,15 @@ int main(void) {
     SOLUTION_HEAPVEC_1
     PRINT_RESULT_VECTOR(c1, "c1")
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    gettimeofday(&temp_1, (struct timezone *)0);
+    int *c1 = (int *)malloc(LEN * sizeof(int));
+    heap_vecs_1_aux(a, b, c1);
+    gettimeofday(&temp_2, (struct timezone *)0);
+    CPU_times[1] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    PRINT_RESULT_VECTOR(c1, "c1");
 
 #endif
     // ---------------------- Heap vectors 2 -----------------------
@@ -114,10 +194,15 @@ int main(void) {
     SOLUTION_HEAPVEC_2
     PRINT_RESULT_VECTOR(c2, "c2")
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    gettimeofday(&temp_1, (struct timezone *)0);
+    int *c2;
+    heap_vecs_2_aux(a, b, &c2);
+    gettimeofday(&temp_2, (struct timezone *)0);
+    CPU_times[2] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    PRINT_RESULT_VECTOR(c2, "c2");
 
 #endif
     // ---------------------- Stack vectors 2 ----------------------
@@ -127,13 +212,17 @@ int main(void) {
 #ifdef RESULTS
     SOLUTION_STACKVEC_2
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    // NOT ALLOWED
+    // gettimeofday(&temp_1, (struct timezone *)0);
+    // int *c3 = heap_vecs_3_aux(a, b);
+    // gettimeofday(&temp_2, (struct timezone *)0);
+    // CPU_times[3] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    // PRINT_RESULT_VECTOR(c3, "c3");
 
 #endif
-
 
     /* Now, do the same 4 previous exercises but with the three
      * matrices A, B, C. All the matrices has N rows and M columns.
@@ -147,55 +236,79 @@ int main(void) {
      */
     // --------------------- Stack matrices ----------------------
 #ifdef RESULTS
-        SOLUTION_STACKMAT_1
-        PRINT_RESULT_MATRIX(((int*)C), "C")
+    SOLUTION_STACKMAT_1
+    PRINT_RESULT_MATRIX(((int *)C), "C")
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    gettimeofday(&temp_1, (struct timezone *)0);
+    int A[N * M], B[N * M], C[N * M];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+        {
+            A[i * M + j] = i + j;
+            B[i * M + j] = 100 * (i + j);
+        }
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            C[i * M + j] = A[i * M + j] + B[i * M + j];
 
+    gettimeofday(&temp_2, (struct timezone *)0);
+    CPU_times[4] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    PRINT_RESULT_MATRIX(C, "C");
 
 #endif
 
     // --------------------- Heap matrices -----------------------
 #ifdef RESULTS
-        SOLUTION_HEAPMAT_1
-        PRINT_RESULT_MATRIX(C1, "C1")
+    SOLUTION_HEAPMAT_1
+    PRINT_RESULT_MATRIX(C1, "C1")
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    gettimeofday(&temp_1, (struct timezone *)0);
+    int *C1 = (int *)malloc(N * M * sizeof(int));
+    heap_mat_1_aux(A, B, C1);
+    gettimeofday(&temp_2, (struct timezone *)0);
+    CPU_times[5] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    PRINT_RESULT_MATRIX(C1, "C1");
 
 #endif
 
     // --------------------- Heap matrices -----------------------
 #ifdef RESULTS
-        SOLUTION_HEAPMAT_2
-        PRINT_RESULT_MATRIX(C2, "C2")
+    SOLUTION_HEAPMAT_2
+    PRINT_RESULT_MATRIX(C2, "C2")
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
+    gettimeofday(&temp_1, (struct timezone *)0);
+    int *C2;
+    heap_mat_2_aux(A, B, &C2);
+    gettimeofday(&temp_2, (struct timezone *)0);
+    CPU_times[6] = ((temp_2.tv_sec - temp_1.tv_sec) + (temp_2.tv_usec - temp_1.tv_usec) / 1000000.0);
+    PRINT_RESULT_MATRIX(C2, "C2");
+    
 #endif
 
     // --------------------- Stack matrices ----------------------
 #ifdef RESULTS
-        SOLUTION_STACKMAT_2
+    SOLUTION_STACKMAT_2
 #else
-        /* |========================================| */
-        /* |           Put here your code           | */
-        /* |========================================| */
-
+    /* |========================================| */
+    /* |           Put here your code           | */
+    /* |========================================| */
 
 #endif
 
-    for (int i=0; i<NPROBS; i++) {
+    for (int i = 0; i < NPROBS; i++)
+    {
         printf("Problem %d runs in %9.8f CPU time\n", i, CPU_times[i]);
     }
     printf("\n");
 
-    return(0);
+    return (0);
 }
